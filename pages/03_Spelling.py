26ecd01e5fbe8386354f728b3c87851bcc65aea7
andehr <andy.d.robertson@gmail.com>
2023-11-26 23:22:36 +0000

    spelling

---
+++ pages/03_Spelling.py
@@ -0,0 +1,67 @@
+import random
+import time
+from typing import Dict, List, Optional
+
+import streamlit as st
+from streamlithelpers import SessionObject
+
+from images import get_image_data
+
+st.set_page_config(layout="centered")
+
+MODE_COMMON = "common name"
+MODE_LATIN = "latin name"
+
+
+@SessionObject("current_spelling_flower")
+def current_flower(s: Dict) -> Dict:
+    return s
+
+
+@SessionObject("current_revealed")
+def current_revealed(r: Optional[List[int]]) -> Optional[List[int]]:
+    return r
+
+
+def reveal_letter(name):
+    revealed = current_revealed.get()
+    if len(revealed) == len(name):
+        return revealed
+    options = [i for i in range(len(name)) if i not in revealed]
+    choice = random.choice(options)
+    return revealed + [choice]
+
+
+current_revealed.init([])
+
+
+image_data = get_image_data()
+
+mode = st.selectbox("Mode", options=[MODE_COMMON, MODE_LATIN])
+
+if st.button("Start", use_container_width=True):
+    current_flower(random.choice(image_data))
+    current_revealed([])
+
+if flower := current_flower.get():
+    st.header(f"How do you spelling the {mode} of the following flower?")
+    st.image(flower["image"])
+
+    name = flower["common"] if mode == MODE_COMMON else flower["latin"]
+
+    if st.sidebar.button("Hint", use_container_width=True):
+        current_revealed(reveal_letter(name))
+
+    revealed = current_revealed.get()
+    letter_cols = st.columns(len(name))
+
+    for i, (letter, col) in enumerate(zip(name, letter_cols)):
+        with col:
+            if letter == " ":
+                st.text(" ")
+            elif i in revealed:
+                st.text(letter)
+            else:
+                st.text("*")
+
+    answer = st.text_input("Answer")
