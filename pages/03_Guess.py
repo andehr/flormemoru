097e114979c8386d11b6d2be75bcdb3f513b4d86
Andy <andy.d.robertson@gmail.com>
2023-11-29 19:18:21 +0000

    reset answer after guessing

--- pages/03_Guess.py
+++ pages/03_Guess.py
@@ -1,7 +1,7 @@
 import random
 
 import streamlit as st
-from streamlithelpers import SessionObject
+from streamlithelpers import SessionObject, get_state, set_state
 
 from images import get_image_data
 
@@ -19,12 +19,13 @@ st.title("Guess the flower")
 
 if st.button("Guess!", use_container_width=True, type="primary"):
     current_image(random.choice(image_data))
+    set_state("answer_toggle", False)
 
 if image_dict := current_image.get():
 
     st.image(image_dict["image"])
 
-if st.toggle("Show answer"):
+if st.toggle("Show answer", value=get_state("answer_toggle"), key="answer_toggle"):
     st.header(image_dict["common"])
     st.subheader(f"_{image_dict['latin']}_")
 
